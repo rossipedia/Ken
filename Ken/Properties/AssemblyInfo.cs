27f255455af6ee8fc86bf1dc880b107469feb1d2
Bryan J. Ross <bryan@rossipedia.com>
2013-05-27 11:30:59 -0500

    Add SLN, Project, and License

---
+++ Ken/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ken")]
+[assembly: AssemblyDescription("Ken Parser Framework")]
+[assembly: AssemblyCompany("Bryan J Ross")]
+[assembly: AssemblyProduct("Ken")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1772eced-a707-4d1d-ac1c-8250e279ab37")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0*")]
+[Assembly: InternalsVisibleTo("Ken.Tests")]
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-ken
+Ken
 ===
 
 A parser framework
+
+Inspired by the [Sprache][1] parser framework for C#.
+
+[1]: https://github.com/sprache/Sprache/
